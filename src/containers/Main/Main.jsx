5618b4fe8f7a02ef49414b7c178ceb5ce43fea5a
Max Silver <Maxbsilver@gmail.com>
2019-08-08 14:17:33 -0600

    Add header component and shift the logo to thee header component from main

---
+++ src/containers/Main/Main.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import Header from '../../components/Header/Header';
+export class Main extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       
+    }
+  }
+  
+  componentDidMount() {
+    
+  }
+  
+  render() {
+    return (
+      <main>
+       <Header/>
+        
+      </main>
+    )
+  }
+}
+
+export default Main

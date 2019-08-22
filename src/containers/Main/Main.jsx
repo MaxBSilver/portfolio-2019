7751cebb5380a97fd37d3911bd90fe9decf0a5c2
Max Silver <Maxbsilver@gmail.com>
2019-08-22 11:02:09 -0600

    basic styling for main page

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -1,25 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Header from '../../components/Header/Header';
 export class Main extends Component {
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-       
-    }
-  }
-  
-  componentDidMount() {
-    
-  }
-  
-  render() {
-    return (
-      <main>
-        
-      </main>
-    )
-  }
+	constructor (props) {
+		super(props);
+
+		this.state = {};
+	}
+
+	componentDidMount () {}
+
+	render () {
+		return (
+			<main>
+				<div className="main-center-container">
+					<h1>I'm Max</h1>
+          <h2>a software engineer</h2>
+				</div>
+			</main>
+		);
+	}
 }
 
-export default Main
+export default Main;

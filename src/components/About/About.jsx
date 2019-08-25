fbb662755ead70f43cff4b0131ea105d97694e39
Max Silver <Maxbsilver@gmail.com>
2019-08-25 15:20:56 -0600

    add bio to about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,8 +7,10 @@ export class About extends Component {
 			<div className="about-container">
 				<Header componentName={'About'}> </Header>
 				<section className="about-section abt-sec-1">
-					<img src={portfolio} alt="Portfolio icon" />
-          <p>filler dsjkafgjkdsjkgjkajksn  dasjkfgdjksagdkjdsjka djskgjkdasjkgjkasjkgd jkdsgjkadskjgkjadsgkj</p>
+					<div>
+						<img src={portfolio} alt="Portfolio icon" />
+					</div>
+					<p>Full stack developer primarily leveraging JavaScript to creatively build dynamic applications that are responsive and intuitive. My academic and personal aptitudes are centered around perceptive UX effects, animations with depth, and creating user experiences that feel native at first touch. I enjoy collaborating across the project team and end-users to design and build unique and instinctive experiences.</p>
 				</section>
 				<section className="about-section abt-sec-2">b</section>
 				<section className="about-section abt-sec-3">c</section>

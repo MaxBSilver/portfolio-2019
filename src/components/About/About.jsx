fe3ba448994d498920c7878269fae9d1c1f379b4
Max Silver <Maxbsilver@gmail.com>
2019-08-25 15:07:51 -0600

    reconstruct general html for components to be more similar

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,17 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Header from '../Header/Header';
-
+import portfolio from '../../assets/envio-19.jpg';
 export class About extends Component {
-  render() {
-    return (
-      <div className="about-container">
-        <Header componentName={'About'}> </Header>
-        <section className="about-section" >a</section>
-        <section className="about-section" >b</section>
-        <section className="about-section" >c</section>
-      </div>
-    )
-  }
+	render () {
+		return (
+			<div className="about-container">
+				<Header componentName={'About'}> </Header>
+				<section className="about-section abt-sec-1">
+					<img src={portfolio} alt="Portfolio icon" />
+          <p>filler dsjkafgjkdsjkgjkajksn  dasjkfgdjksagdkjdsjka djskgjkdasjkgjkasjkgd jkdsgjkadskjgkjadsgkj</p>
+				</section>
+				<section className="about-section abt-sec-2">b</section>
+				<section className="about-section abt-sec-3">c</section>
+			</div>
+		);
+	}
 }
 
-export default About
+export default About;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default function Header (props){
 	return (
-			<section className="header-main">
+			<div className="header-main">
 				<header className="fade-in">
 					<h2>{props.componentName}</h2>
 				</header>
-			</section>
+			</div>
 	);
 }

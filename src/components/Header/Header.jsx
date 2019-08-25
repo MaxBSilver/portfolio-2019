902cd1f38ea41a75c72e505c5bb0b4629a5db3a9
Max Silver <Maxbsilver@gmail.com>
2019-08-25 13:59:25 -0600

    add image to header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 
 export default function Header (props){
 	return (
-		<header className="fade-in">
-			<h2>{props.componentName}</h2>
-		</header>
+		<div>
+			<section className="header-background">
+				<img
+					src={
+						'https://jonathanmh.com/wp-content/uploads/2017/05/best-visual-studio-code-dark-syntax-themes-1200x433.png'
+					}
+					alt="code background"
+				/>
+			</section>
+			<section className="header-main">
+				<header className="fade-in">
+					<h2>{props.componentName}</h2>
+				</header>
+			</section>
+		</div>
 	);
 }

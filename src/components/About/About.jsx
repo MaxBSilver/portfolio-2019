de586799c06a051db5775053304d11eeb9a7f583
Max Silver <Maxbsilver@gmail.com>
2019-09-05 07:59:08 -0600

    setup notes folder and inner files

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,10 +7,11 @@ export class About extends Component {
 			<div className="about-container">
 				<Header componentName={'About'}> </Header>
 				<section className="about-section abt-sec-1">
+          <h3>Who am I?</h3>
 					<div>
 						<img src={portfolio} alt="Portfolio icon" />
 					</div>
-					<p>Full stack developer primarily leveraging JavaScript to creatively build dynamic applications that are responsive and intuitive. My academic and personal aptitudes are centered around perceptive UX effects, animations with depth, and creating user experiences that feel native at first touch. I enjoy collaborating across the project team and end-users to design and build unique and instinctive experiences.</p>
+					<p>Full stack developer primarily leveraging JavaScript to creatively build dynamic applications that are both responsive and intuitive. My academic and personal aptitudes are centered around perceptive UX effects, animations with depth, and creating user experiences that feel native at first touch. I enjoy collaborating across the project team and end-users to design and build unique and instinctive experiences.</p>
 				</section>
 				<section className="about-section abt-sec-2">b</section>
 				<section className="about-section abt-sec-3">c</section>

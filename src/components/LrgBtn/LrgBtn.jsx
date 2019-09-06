7fcf795b0e5ac4f2182cedf61138eae6425d8c0c
Max Silver <Maxbsilver@gmail.com>
2019-09-06 08:37:00 -0400

    add background images to buttons on main

--- src/components/LrgBtn/LrgBtn.jsx
+++ src/components/LrgBtn/LrgBtn.jsx
@@ -2,17 +2,20 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { updateNav } from '../../redux/actions/index';
+import About from '../../assets/fort.png' ;
 
 export class LrgButton extends Component {
+
 	handleClick = e => {
 		this.props.updateNav(e.target.name);
 		localStorage.setItem('nav', e.target.name);
 	};
+
 	render () {
 		return (
 			<div className="lrg-btn">
-				<NavLink onClick={this.handleClick} to={this.props.btnName}>
-					<button name={this.props.btnName}>
+				<NavLink onClick={this.handleClick} to={this.props.btnName }>
+					<button name={this.props.btnName} style={{backgroundImage: `url(${About})`, backgroundPosition: 'center'}}>
 						<span>{this.props.btnName}</span>
 					</button>
 				</NavLink>

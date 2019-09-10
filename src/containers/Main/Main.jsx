090c8ec2f6b9e81440de7a304352b3ae6cd9c7ad
Max Silver <Maxbsilver@gmail.com>
2019-09-10 15:11:54 -0600

    add new type face to the main component

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -1,20 +1,41 @@
 import React, { Component } from 'react';
-import LrgButton from '../../components/LrgBtn/LrgBtn';
-
+import Typed from 'typed.js';
 import background from '../../assets/team_bkg.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 export class Main extends Component {
+	constructor (props) {
+		super(props);
+
+		this.state = {};
+	}
+	componentDidMount () {
+		const options = {
+			strings: [ 'React.', 'Vue.', 'React Native.', 'Node.', 'PostGreSQL.',  ],
+			typeSpeed: 80,
+			backSpeed: 60,
+			loop: true,
+		};
+		this.typed = new Typed(this.el, options);
+	}
+
 	render () {
 		return (
 			<main>
 				<div className="main-parent-container">
 					<section className="info-container">
 						<section>
-							<h1>I'm Max</h1>
-							<h2>a software engineer</h2>
+							<span>
+								I build applications <br/> with{' '}
+								<span
+									style={{ whiteSpace: 'pre' }}
+									ref={el => {
+										this.el = el;
+									}}
+								/>
+							</span>
 							<div>
 								<a href="https://github.com/MaxBSilver" target="_blank" rel="noopener noreferrer">
 									<FontAwesomeIcon icon={faGithubSquare} />
@@ -28,9 +49,7 @@ export class Main extends Component {
 							</div>
 						</section>
 					</section>
-					<section className="image-container" style={{backgroundImage: `url(${background})`}}>
-						{/* <img src={background} alt="background landscape, green field with dark green trees" /> */}
-					</section>
+					<section className="image-container" style={{ backgroundImage: `url(${background})` }} />
 				</div>
 			</main>
 		);

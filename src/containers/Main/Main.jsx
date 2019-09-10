aa1d3a100fba78d12b13052a5210476b43f81799
Max Silver <Maxbsilver@gmail.com>
2019-09-10 12:59:30 -0600

    Update to new design inspiration for main, some JSX changes but mainly scss

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import LrgButton from '../../components/LrgBtn/LrgBtn';
 
-import background from '../../assets/2.png';
+import background from '../../assets/team_bkg.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
@@ -12,33 +12,25 @@ export class Main extends Component {
 			<main>
 				<div className="main-parent-container">
 					<section className="info-container">
-						<h1>I'm Max</h1>
-						<h2>a software engineer</h2>
-						<a href="https://github.com/MaxBSilver" target="_blank" rel="noopener noreferrer">
-							<FontAwesomeIcon icon={faGithubSquare} />
-						</a>
-						<a href="https://www.linkedin.com/in/max-b-silver/" target="_blank" rel="noopener noreferrer">
-							<FontAwesomeIcon icon={faLinkedin} />
-						</a>
-						<FontAwesomeIcon icon={faEnvelope} />
-					</section>
-					<section className="image-container">
-						<img src={background} alt="background landscape, green field with dark green trees" />
-					</section>
-					{/* <section className="info-container">
-		
-							<section className="icon-container">
-								<a href="https://github.com/MaxBSilver" target="_blank" rel="noopener noreferrer" >
+						<section>
+							<h1>I'm Max</h1>
+							<h2>a software engineer</h2>
+							<div>
+								<a href="https://github.com/MaxBSilver" target="_blank" rel="noopener noreferrer">
 									<FontAwesomeIcon icon={faGithubSquare} />
 								</a>
-								<a href="https://www.linkedin.com/in/max-b-silver/" target="_blank" rel="noopener noreferrer" >
+								<a href="https://www.linkedin.com/in/max-b-silver/" target="_blank" rel="noopener noreferrer">
 									<FontAwesomeIcon icon={faLinkedin} />
 								</a>
-								<FontAwesomeIcon icon={faEnvelope} />
-							</section>
+								<a href="https://www.linkedin.com/in/max-b-silver/" target="_blank" rel="noopener noreferrer">
+									<FontAwesomeIcon icon={faEnvelope} />
+								</a>
+							</div>
 						</section>
-						<section className="main-btn-cntr" />
-						<img src={background} alt="background landscape, green field with dark green trees" /> */}
+					</section>
+					<section className="image-container" style={{backgroundImage: `url(${background})`}}>
+						{/* <img src={background} alt="background landscape, green field with dark green trees" /> */}
+					</section>
 				</div>
 			</main>
 		);

b59a29b7919641970e538847f3e29085dda9ab2b
Max Silver <Maxbsilver@gmail.com>
2019-09-10 16:54:54 -0600

    add new image and update general styling

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -1,22 +1,18 @@
 import React, { Component } from 'react';
 import Typed from 'typed.js';
-import background from '../../assets/team_bkg.png';
+import background from '../../assets/2.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 export class Main extends Component {
-	constructor (props) {
-		super(props);
 
-		this.state = {};
-	}
 	componentDidMount () {
 		const options = {
-			strings: [ 'React.', 'Vue.', 'React Native.', 'Node.', 'PostGreSQL.',  ],
+			strings: [ 'React.', 'React Native.', 'Vue.', 'Node.', 'PostGreSQL.' ],
 			typeSpeed: 80,
 			backSpeed: 60,
-			loop: true,
+			loop: true
 		};
 		this.typed = new Typed(this.el, options);
 	}
@@ -25,10 +21,10 @@ export class Main extends Component {
 		return (
 			<main>
 				<div className="main-parent-container">
-					<section className="info-container">
+					<section className="info-container image-container" style={{ backgroundImage: `url(${background})` }}>
 						<section>
 							<span>
-								I build applications <br/> with{' '}
+								I build applications <br /> with{' '}
 								<span
 									style={{ whiteSpace: 'pre' }}
 									ref={el => {
@@ -36,6 +32,10 @@ export class Main extends Component {
 									}}
 								/>
 							</span>
+							<p>
+								Developer primarily leveraging JavaScript to creatively build dynamic applications that are both responsive
+								and intuitive.
+							</p>
 							<div>
 								<a href="https://github.com/MaxBSilver" target="_blank" rel="noopener noreferrer">
 									<FontAwesomeIcon icon={faGithubSquare} />
@@ -48,8 +48,9 @@ export class Main extends Component {
 								</a>
 							</div>
 						</section>
+					
 					</section>
-					<section className="image-container" style={{ backgroundImage: `url(${background})` }} />
+					{/* <section className="image-container" style={{ backgroundImage: `url(${background})` }} /> */}
 				</div>
 			</main>
 		);

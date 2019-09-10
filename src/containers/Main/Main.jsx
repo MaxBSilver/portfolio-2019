59e32dd282362bd679075c0cd07ee62ec74bfd0f
Max Silver <Maxbsilver@gmail.com>
2019-09-10 11:27:30 -0600

    refactor scss for main component, update style to new wireframe

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -1,31 +1,44 @@
 import React, { Component } from 'react';
 import LrgButton from '../../components/LrgBtn/LrgBtn';
 
-import Landscape from '../../assets/119.jpg';
+import background from '../../assets/2.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 export class Main extends Component {
 	render () {
 		return (
 			<main>
 				<div className="main-parent-container">
-					<div className="main-top">
+					<section className="info-container">
 						<h1>I'm Max</h1>
-					</div>
-					<div className="main-bottom">
 						<h2>a software engineer</h2>
-						<section className="icon-container">
-							<a href="https://github.com/MaxBSilver" target="_blank"><FontAwesomeIcon icon={faGithubSquare} /></a>
-							<a href="https://www.linkedin.com/in/max-b-silver/" target="_blank"><FontAwesomeIcon icon={faLinkedin} /></a>
-							<FontAwesomeIcon icon={faEnvelope} />
-
+						<a href="https://github.com/MaxBSilver" target="_blank" rel="noopener noreferrer">
+							<FontAwesomeIcon icon={faGithubSquare} />
+						</a>
+						<a href="https://www.linkedin.com/in/max-b-silver/" target="_blank" rel="noopener noreferrer">
+							<FontAwesomeIcon icon={faLinkedin} />
+						</a>
+						<FontAwesomeIcon icon={faEnvelope} />
+					</section>
+					<section className="image-container">
+						<img src={background} alt="background landscape, green field with dark green trees" />
+					</section>
+					{/* <section className="info-container">
+		
+							<section className="icon-container">
+								<a href="https://github.com/MaxBSilver" target="_blank" rel="noopener noreferrer" >
+									<FontAwesomeIcon icon={faGithubSquare} />
+								</a>
+								<a href="https://www.linkedin.com/in/max-b-silver/" target="_blank" rel="noopener noreferrer" >
+									<FontAwesomeIcon icon={faLinkedin} />
+								</a>
+								<FontAwesomeIcon icon={faEnvelope} />
+							</section>
 						</section>
-						<section className="main-btn-cntr">
-							<img src={Landscape} alt="background landscape, green field with dark green trees" />
-						</section>
-					</div>
+						<section className="main-btn-cntr" />
+						<img src={background} alt="background landscape, green field with dark green trees" /> */}
 				</div>
 			</main>
 		);

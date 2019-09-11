5e8713d620146d59a1b8add8ca77decfa63b149d
Max Silver <Maxbsilver@gmail.com>
2019-09-11 16:08:07 -0600

    add about sections for 1 and 2

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Header from '../Header/Header';
 import about from '../../assets/about.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faReact, faNodeJs, faSass, faVuejs, faAmazon, faGithub, faLess } from '@fortawesome/free-brands-svg-icons';
 export class About extends Component {
 	render () {
 		return (
@@ -19,11 +21,19 @@ export class About extends Component {
 					</div>
 				</section>
 				<section className="about-section abt-sec-2">
-					<h6>Who am I?</h6>
-					<p>
-						I'm a web designer / developer based in Denver, Colorado. I have a passion for web design and love to create
-						for web and mobile devices.
-					</p>
+					<h6>My Technologies</h6>
+					<div>
+						<p>Mainly working with JavaScript, I'm highly proficient with most frameworks including React.js, React Native, Vue.js, and Angular. I have worked with Node.js and Java on the backend and SQL and Redis for data basing.</p>
+						<div className="icon-container">
+							<FontAwesomeIcon className="react" icon={faReact} />
+							<FontAwesomeIcon className="node" icon={faNodeJs} />
+							<FontAwesomeIcon className="vue" icon={faVuejs} />
+							<FontAwesomeIcon className="sass" icon={faSass} />
+							<FontAwesomeIcon className="less" icon={faLess} />
+							<FontAwesomeIcon className="github" icon={faGithub} />
+							{/* <FontAwesomeIcon className="amazon" icon={faAmazon} /> */}
+						</div>
+					</div>
 				</section>
 				<section className="about-section abt-sec-3">
 					<h6>Who am I?</h6>

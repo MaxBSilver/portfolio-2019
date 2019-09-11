22ec91743fc62b3d5e2f4718509b9335d186650a
Max Silver <Maxbsilver@gmail.com>
2019-09-11 15:30:13 -0600

    add fix for height spacing over main and about sections, add first about section and begin creating the template for the other 2

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Header from '../Header/Header';
-import coding from '../../assets/codingisometric-2.png';
+import about from '../../assets/about.png';
 export class About extends Component {
 	render () {
 		return (
@@ -8,14 +8,30 @@ export class About extends Component {
 				<Header componentName={'About'}> </Header>
 				<section className="about-section abt-sec-1">
 					<div>
-						<img src={coding} alt="Portfolio icon" />
+						<img src={about} alt="Portfolio icon" />
 					</div>
-          <h3>Who am I?</h3>
-
-					<p>Full stack developer primarily leveraging JavaScript to creatively build dynamic applications that are both responsive and intuitive. My academic and personal aptitudes are centered around perceptive UX effects, animations with depth, and creating user experiences that feel native at first touch. I enjoy collaborating across the project team and end-users to design and build unique and instinctive experiences.</p>
+					<div>
+						<h6>Who am I?</h6>
+						<p>
+							I'm a web designer / developer based in Denver, Colorado. I have a passion for web design and love to
+							create for web and mobile devices.
+						</p>
+					</div>
 				</section>
-				<section className="about-section abt-sec-2">b</section>
-				<section className="about-section abt-sec-3">c</section>
+				<section className="about-section abt-sec-2">
+					<h6>Who am I?</h6>
+					<p>
+						I'm a web designer / developer based in Denver, Colorado. I have a passion for web design and love to create
+						for web and mobile devices.
+					</p>
+				</section>
+				<section className="about-section abt-sec-3">
+					<h6>Who am I?</h6>
+					<p>
+						I'm a web designer / developer based in Denver, Colorado. I have a passion for web design and love to create
+						for web and mobile devices.
+					</p>
+				</section>
 			</div>
 		);
 	}

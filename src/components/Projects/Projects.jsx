15d16c8be5a2636ec1fb8a6e9b21b51fe3618b55
Max Silver <Maxbsilver@gmail.com>
2019-10-01 11:47:40 -0600

    add minor change to projects component invlolving minor classnames

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,14 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Header from '../Header/Header';
-
+import projectImg from '../../assets/envio-19.jpg';
+import hexbrush from '../../assets/hexbrush.png';
 export class Projects extends Component {
-  render() {
-    return (
-      <div className="projects">
-        <Header componentName={'Projects'}/>        
-      </div>
-    )
-  }
+	render () {
+		return (
+			<div className="projects">
+				<Header componentName={'Projects'} />
+				<section className="sec-1">
+					<img src={projectImg} />
+					<h3>What I've been up to.</h3>
+					<p>Here's a selection of recent projects and designs. </p>
+				</section>
+				<section className="project-sec sec-2">
+					<div>
+						<img src={hexbrush} />
+					</div>
+          <div>
+            <p></p>
+            <button>View Repo</button>
+          </div>
+				</section>
+			</div>
+		);
+	}
 }
 
-export default Projects
+export default Projects;
